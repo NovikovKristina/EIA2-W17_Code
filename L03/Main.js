04cc394a59ada744c555105102fa062456701ead
Jirka <del@hs-furtwangen.de>
2017-11-13 18:10:05 +0100

    Fehler im Einzel-Interface-Beispiel behoben. Animate wurde zu früh aufgerufen

--- L03/Main.js
+++ L03/Main.js
@@ -1,8 +1,9 @@
-var L04_MultiDim;
-(function (L04_MultiDim) {
+var L03;
+(function (L03) {
     window.addEventListener("load", init);
     let crc2;
-    let squares = [];
+    let arrayX = [];
+    let arrayY = [];
     function init() {
         let canvas = document.getElementsByTagName("canvas")[0];
         console.log(canvas);
@@ -11,20 +12,21 @@ var L04_MultiDim;
         console.log("setTimeout");
         animate();
         for (let i = 0; i < 100; i++) {
-            squares[i] = [200, 150];
+            arrayX[i] = 200;
+            arrayY[i] = 150;
         }
         // hier Hintergrund speichern
     }
     function animate() {
         console.log("Timeout");
         crc2.clearRect(0, 0, 400, 300); // hier Hintergrund restaurieren
-        for (let i = 0; i < squares.length; i++) {
-            squares[i][0] += Math.random() * 4 - 2; // hier experimentieren um
-            squares[i][1] += Math.random() * 4 - 2; // andere Bewegungsmuster zu finden
+        for (let i = 0; i < arrayX.length; i++) {
+            arrayX[i] += Math.random() * 4 - 2; // hier experimentieren um
+            arrayY[i] += Math.random() * 4 - 2; // andere Bewegungsmuster zu finden
             crc2.fillStyle = "#ff0000";
-            crc2.fillRect(squares[i][0], squares[i][1], 20, 20);
+            crc2.fillRect(arrayX[i], arrayY[i], 20, 20);
         }
         window.setTimeout(animate, 20);
     }
-})(L04_MultiDim || (L04_MultiDim = {}));
+})(L03 || (L03 = {}));
 //# sourceMappingURL=Main.js.map
